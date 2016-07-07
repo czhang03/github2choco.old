5cfbd37cc592b71ae6c66d13758287fe734b1e4f
Cheng Zhang <13501393281@163.com>
2016-07-07 13:15:14 -0400

    create the basic structure of the program

--- github2choco.ps1
+++ github2choco.ps1
@@ -1,14 +1,28 @@
-function New-Package ($release, $version, $releaseNote, $description) {
-	# create the path
-	$newPackagePath = "$packagePath\$version"
-	# use out-null to redirect the output to null. (do not show out put)
-	New-Item $newPackagePath -ItemType Directory -Force -Confirm:$false | Out-Null
-	New-Item "$newPackagePath\tools" -ItemType Directory -Force -Confirm:$false | Out-Null
-	
-	# create install scripts
-	New-Tools -path "$newPackagePath\tools" -release $release
-	New-NuspecFile -path $newPackagePath -version $version -releaseNote $releaseNote -description $description
+param(
+    [string] $command,
+    [string] $githubRepo,
+    [switch] $All,
+    [switch] $Force,
+	[string] $packagePath,
+	[string] $templatePath,
+	[string] $packageName,
+	[switch] $Alpha,
+	[switch] $Nightly
+)
 
-	# change the latest version
-	$version | Out-File "$packagePath\latest_version.txt" -Encoding utf8
+# set the workspace location
+$workspaceLocation = Split-Path $myInvocation.MyCommand.Definition -Parent
+
+if (-Not ($packageName)){
+	$packageName = $($githubRepo -split '/')[1]
 }
+if (-Not ($packagePath)) {
+	$packagePath = "$HOME\github2chocoPackages\$packageName-choco"
+}
+if (-Not ($packagePath)) {
+	$templatePath = "$packagePath\$packageName"
+}
+
+# load modules
+. $workspaceLocation\zipPackageWriter.ps1
+
--- zipPackageWriter.ps1
+++ zipPackageWriter.ps1
@@ -19,13 +19,13 @@ $workspaceLocation = Split-Path $myInvocation.MyCommand.Definition -Parent
 $packagePath = [System.IO.Path]::GetFullPath($packagePath)
 $templatePath = [System.IO.Path]::GetFullPath($templatePath)
 
-# translate all the path
+# load defualt value
 $tempDir = "$HOME\AppData\Local\Temp"
-if([string]::IsNullOrEmpty($templatePath)){
+if(-Not ($templatePath)){
 	$templatePath = "$packagePath\template"
 }
 
-if ([string]::IsNullOrEmpty($packageName)) {
+if (-Not ($packageName)) {
 	$packageName = Split-Path -Path $packagePath -Leaf
 }
 
@@ -116,31 +116,41 @@ function New-Tools ($Path, $release) {
 	$installStr | Out-File "$Path\chocolateyinstall.ps1" -Encoding utf8
 }
 
+function New-Package ($release, $version, $releaseNote, $description) {
+	# create the path
+	$newPackagePath = "$packagePath\$version"
+	# use out-null to redirect the output to null. (do not show out put)
+	New-Item $newPackagePath -ItemType Directory -Force -Confirm:$false | Out-Null
+	New-Item "$newPackagePath\tools" -ItemType Directory -Force -Confirm:$false | Out-Null
+	
+	# create install scripts
+	New-Tools -path "$newPackagePath\tools" -release $release
+	New-NuspecFile -path $newPackagePath -version $version -releaseNote $releaseNote -description $description
 
+	# change the latest version
+	$version | Out-File "$packagePath\latest_version.txt" -Encoding utf8
+}
 
-
-
-######################################################
-# MAIN
-######################################################
-$release = Get-RemoteRelease
-$remoteVersion = Get-RemoteVersion -remoteRelease $release
-$localVersion = Get-LocalVersion
-if (-Not $Force) {
-	if($remoteVersion -ne $localVersion) {
+function Write-ZipPackage {
+	$release = Get-RemoteRelease
+	$remoteVersion = Get-RemoteVersion -remoteRelease $release
+	$localVersion = Get-LocalVersion
+	if (-Not $Force) {
+		if($remoteVersion -ne $localVersion) {
+			$releaseNote = $release.body.replace("\n", "`r`n")
+			$nuspecTemplate = Get-NuspecTemplate
+			$description = $nuspecTemplate.package.metadata.description
+			New-Package -release $release -version $remoteVersion -releaseNote $releaseNote -description $description
+		}
+		else {
+			Write-Host 'remote and local version match, exiting...' -ForegroundColor Green
+		}
+	}
+	else {
+		Write-Warning 'Force executing'
 		$releaseNote = $release.body.replace("\n", "`r`n")
 		$nuspecTemplate = Get-NuspecTemplate
 		$description = $nuspecTemplate.package.metadata.description
 		New-Package -release $release -version $remoteVersion -releaseNote $releaseNote -description $description
 	}
-	else {
-		Write-Host 'remote and local version match, exiting...' -ForegroundColor Green
-	}
-}
-else {
-	Write-Warning 'Force executing'
-	$releaseNote = $release.body.replace("\n", "`r`n")
-	$nuspecTemplate = Get-NuspecTemplate
-	$description = $nuspecTemplate.package.metadata.description
-	New-Package -release $release -version $remoteVersion -releaseNote $releaseNote -description $description
 }
