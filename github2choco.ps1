387edf070a1bd9977ce7ae762f8b992a2d5bd1d4
Cheng Zhang <13501393281@163.com>
2016-07-08 19:48:55 -0400

    change the parameters for better commandline usage

--- github2choco.ps1
+++ github2choco.ps1
@@ -1,7 +1,6 @@
 param(
     [string] $command,
-    [string] $githubRepo,
-	[string] $packageName,
+	[string] $package,
 	[string] $packageType,
     [switch] $All,
     [switch] $Force,
@@ -9,31 +8,43 @@ param(
 	[switch] $Nightly
 )
 
+$ErrorActionPreference = 'stop'
+
 # set the workspace location
 $workspaceLocation = Split-Path $myInvocation.MyCommand.Definition -Parent
-
-# load defualt value
-if (-Not ($packageName)){
-	$packageName = $($githubRepo -split '/')[1]
-}
-if (-Not ($packagePath)) {
-	$packagePath = "$HOME\github2chocoPackages\$packageName-choco"
-}
-if (-Not ($packagePath)) {
-	$templatePath = "$packagePath\$packageName"
-}
-
-# turn all the path to full path to avoid confusion
-$packagePath = [System.IO.Path]::GetFullPath($packagePath)
-$templatePath = [System.IO.Path]::GetFullPath($templatePath)
-
 
 # load modules
 . $workspaceLocation\zipPackageWriter.ps1
 
+# create a new choco package
+if ($command -eq 'new') {
+	# package is the github repo name
+	$githubRepo = $package
+
+	# load defualt value
+	if (-Not ($packageName)){
+	$packageName = $($githubRepo -split '/')[1]
+	}
+	if (-Not ($packagePath)) {
+		$packagePath = "$HOME\github2chocoPackages\$packageName-choco"
+	}
+	if (-Not ($packagePath)) {
+		$templatePath = "$packagePath\$packageName"
+	}
+
+	# turn all the path to full path to avoid confusion
+	$packagePath = [System.IO.Path]::GetFullPath($packagePath)
+	$templatePath = [System.IO.Path]::GetFullPath($templatePath)
+
+}
 
 # update zip package
 if ($command -eq 'update') {
-	Write-ZipPackage -packageName $packageName
+	# package is the package name, not github repo name
+	$packageName = $package
+
+	# do the work here
+	Update-ZipPackage -packageName $packageName -Force $Force
+	
 }
 
