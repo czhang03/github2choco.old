451c63dac78fdc98b9f4ee95f23a4636e1043913
Cheng Zhang <13501393281@163.com>
2016-07-08 19:49:35 -0400

    remove redundent and move get-nuspacetemplate to numspace gen

--- localHandler.ps1
+++ localHandler.ps1
@@ -1,10 +1,10 @@
 function Read-LocalPrfile {
 	Write-Host ''
-	$profilePath = Split-Path $myInvocation.MyCommand.Definition -Parent
+	$profilePath = $workspaceLocation
 	$profileFullName = "$profilePath\profile.json"
 	if (Test-Path $profileFullName) {
 		Write-Host 'profile found' -ForegroundColor Green	
-		$profile = Get-Content $profileFullName
+		$profile = Get-Content $profileFullName | ConvertFrom-Json
 	}
 	else {
 		Write-Host 'Profile Not Found, starting with an empty profile'
@@ -15,33 +15,10 @@ function Read-LocalPrfile {
 	$profile
 }
 
-function Get-LocalVersion($localProfile, $packageName) {
-	# return
-	$localProfile.$packageName.version
-}
-
-function Get-PackagePath ($localProfile, $packageName) {
-	#return
-	$localProfile.$packageName.packagePath
-}
-
-function Get-GithubRepo ($localRelease, $packageName) {
-	#return
-	$localProfile.$packageName.githubRepo
-}
-
-function Get-NuspecTemplate($profile, $packageName) {
-	$templatePath = $localProfile.$packageName.templatePath
-    $xml = [xml] $(Get-Content "$templatePath\$packageName.nuspec")
-	Write-Host ''
-    Write-Host 'successfull get the template file' -ForegroundColor Green
-    
-    # return
-    $xml
-}
-
 function Save-Profile($localProfile) {
-	$profilePath = Split-Path $myInvocation.MyCommand.Definition -Parent
+	$profilePath = $workspaceLocation
 	$profileFullName = "$profilePath\profile.json"
-	ConvertFrom-Json $localProfile | Out-File $profileFullName -Encoding utf8
+	ConvertTo-Json $localProfile | Out-File $profileFullName -Encoding utf8
+
+	Write-Host 'Profile Successfully saved'
 }
